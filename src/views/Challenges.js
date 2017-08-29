6486d3abb1cfee6fa325dc1d3e49b4bb9bef95e0
TheoVitkovskiy <fvitkovski@mail.de>
2017-08-29 15:09:03 +0200

    Do the design of the Challenges view

--- src/views/Challenges.js
+++ src/views/Challenges.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react';
+import { Card, CardTitle, CardText} from 'material-ui/Card';
 
 class Challenges extends Component {
   render(){
     return(
-      <h1>Challenges Overview</h1>
+      <div className="container">
+        <Card style={{marginBottom: '2em'}}>
+          <CardTitle
+            title="Walk up the mountain with skis on!"
+          />
+          <CardText>
+            29.08.2017
+          </CardText>
+        </Card>
+        <Card style={{marginBottom: '2em'}}>
+          <CardTitle
+            title="Don't walk without your shoes on, ever!"
+          />
+          <CardText>
+            30.08.2017
+          </CardText>
+        </Card>
+        <Card style={{marginBottom: '2em'}}>
+          <CardTitle
+            title="Walk up the mountain with skis on!"
+          />
+          <CardText>
+            31.08.2017
+          </CardText>
+        </Card>
+        <Card style={{marginBottom: '2em'}}>
+          <CardTitle
+            title="Don't walk without your shoes on, ever!"
+          />
+          <CardText>
+            01.09.2017
+          </CardText>
+        </Card>
+        <Card style={{marginBottom: '2em'}}>
+          <CardTitle
+            title="Walk up the mountain with skis on!"
+          />
+          <CardText>
+            02.09.2017
+          </CardText>
+        </Card>
+        <Card style={{marginBottom: '2em'}}>
+          <CardTitle
+            title="Walk up the mountain with skis on!"
+          />
+          <CardText>
+            03.09.2017
+          </CardText>
+        </Card>
+      </div>
     )
   }
 }
