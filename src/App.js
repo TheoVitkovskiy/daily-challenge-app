28b1da13aacad839cfb05a74bbe8e2eaf4691ea8
TheoVitkovskiy <fvitkovski@mail.de>
2017-08-29 06:39:01 +0200

    Add the TopNav with some basic routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React, { Component } from 'react';
-import './App.css';
+import './styles/App.css';
+import './styles/materialize-grid.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 
+import TopNav from './components/TopNav.js';
 
-import AppBar from 'material-ui/AppBar';
+/* Import Views */
+import Home from './views/Home.js'
+
+
+
+
 
 class App extends Component {
   render() {
     return (
-      <MuiThemeProvider>
-        <AppBar
-          title="Daily Challenge App"
-          iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-      </MuiThemeProvider>
+      <BrowserRouter>
+        <MuiThemeProvider>
+          <div>
+            <TopNav />
+
+            <Switch>
+              <Route exact path='/' component={Home} />
+            </Switch>
+          </div>
+        </MuiThemeProvider>
+      </BrowserRouter>
     );
   }
 }
