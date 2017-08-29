715014d3981f32297b8ee0ebe738500116bd2f53
TheoVitkovskiy <fvitkovski@mail.de>
2017-08-29 20:09:42 +0200

    Add progress view with the first attempt at a progress bar

--- src/views/Progress.js
+++ src/views/Progress.js
@@ -1,11 +1,76 @@
 import React, { Component } from 'react';
+import { List } from 'material-ui/List';
+
+
 
 class Progress extends Component {
   render(){
     return(
-      <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row'}}>
-        <h1>Challenge Progress</h1>
-      </div>
+      <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+          <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '2em'}}>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box' style={{backgroundColor: '#76FF03'}}/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+            <div className='box'/>
+
+          </div>
+        </div>
     )
   }
 }
