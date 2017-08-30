3fc01cf1f26c5b0b11ef9da98f67f56e86eefcfa
TheoVitkovskiy <fvitkovski@mail.de>
2017-08-30 11:12:58 +0200

    Change the design of the progress view

--- src/views/Progress.js
+++ src/views/Progress.js
@@ -7,50 +7,119 @@ class Progress extends Component {
   render(){
     return(
       <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
+        <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '2em'}}>
+          <p style={{fontSize: 30}}>Current <span style={{fontWeight: 600}}>Week</span> Progress</p>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '2em'}}>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box' style={{backgroundColor: '#76FF03'}}/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+          <div className='box'/>
+
+          </div>
           <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '2em'}}>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box' style={{backgroundColor: '#76FF03'}}/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
-            <div className='box'/>
+            <p style={{fontSize: 30}}>Current <span style={{fontWeight: 600}}>Month</span> Progress</p>
+          </div>
+          <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '2em'}}>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
+            <div className='box' style={{backgroundColor: '#D500F9'}}/>
             <div className='box'/>
             <div className='box'/>
             <div className='box'/>
@@ -69,7 +138,73 @@ class Progress extends Component {
             <div className='box'/>
             <div className='box'/>
 
-          </div>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '2em'}}>
+              <p style={{fontSize: 30}}>Current <span style={{fontWeight: 600}}>Year</span> Progress</p>
+            </div>
+            <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '2em'}}>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box' style={{backgroundColor: '#FF1744'}}/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+              <div className='box'/>
+
+              </div>
         </div>
     )
   }
