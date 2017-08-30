c06bc5cbd4720e79efa14390d1175336aad66647
TheoVitkovskiy <fvitkovski@mail.de>
2017-08-30 14:21:36 +0200

    Finish the basics of the HomeAlt view, add a CountDown

--- src/views/HomeAlt.js
+++ src/views/HomeAlt.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react'
 import Clock from 'react-live-clock'
 import { ApAnalogClock, ApAnalogClockStyle } from 'apeman-react-clock'
+import Countdown from 'react-countdown-now'
+
+const renderer = ({ hours, minutes, seconds }) => {
+  return <span style={{fontSize: '100px'}}>{hours}:{minutes}:{seconds}</span>
+}
 
 class HomeAlt extends Component {
+
+
   render() {
+    var currentDate = new Date();
+    currentDate.setDate(currentDate.getDate() + 1);
+    var day = currentDate.getDate();
+    var monthNumber = currentDate.getMonth() + 1;
+    var year = currentDate.getFullYear();
+
+    var month = currentDate.toLocaleString("en-us", { month: "short" })
+
     return (
       <div>
-
         <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '2em'}}>
-          <ApAnalogClockStyle scoped width='400px' height='300px' />
-          <ApAnalogClock />
+
+          <p style={{fontSize: 35, textAlign: 'center'}}> Behalte Blickkontakt den ganzen Tag lang </p>
+
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '1em'}}>
+
+          <p style={{fontSize: 30}}> Time till the next Daily! </p>
+
+        </div>
+        <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'row', marginTop: '1em'}}>
+
+          <Countdown
+            date={ day + ' ' + month + ' ' + year + ' ' + '00:00:00'}
+            renderer={renderer}
+          />
+
         </div>
       </div>
     )
