7cdaffeb66a3896b76ba98d6a4fd98b348207333
TheoVitkovskiy <fvitkovski@mail.de>
2017-09-06 08:11:19 +0200

    Make changes to scripts in package.json so the application can run on Heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,28 @@
+const webpack = require('webpack');
+
 module.exports = {
+    devtool: 'source-map'
     entry: './src/index.js',
     output: {
         path: __dirname + '/public',
-        filename: 'bundle.js'
+        filename: 'bundle.js',
+        publicPath: '/public/'
     },
+    plugins: [
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        minimize: true,
+        compress: {
+          warning: false
+        }
+      }),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production'),
+          'API_HOST': 'https://daily-challenge.herukoapp.com'
+        }
+      })
+    ],
     module: {
         loaders: [
             {
