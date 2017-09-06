ec268da492d5747f1f055bc2f64dbc3f9db8fbb1
TheoVitkovskiy <fvitkovski@mail.de>
2017-09-06 10:15:10 +0200

    Make changes to scripts in package.json so the application can run on Heroku

--- tools/publicServer.js
+++ tools/publicServer.js
@@ -3,18 +3,23 @@ import path from 'path';
 import apiRouter from '../api';
 import bodyParser from 'body-parser';
 import open from 'open';
+import favicon from 'serve-favicon';
+import compression from 'compression';
 
 const port = process.env.PORT || 3000;
 const app = express();
 
+app.use(compression());
 app.use(bodyParser.json())
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use('/api', apiRouter)
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(path.join(__dirname, 'assets', 'public', 'favicon.ico')));
 
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '../public/index.html'))
-});
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
+
 
 app.listen(port, function(err) {
   if(err) {
